fc880bf9b30490aa3a602f4cfb470289ea0c6a6d
Roy Binux <root@binux.me>
2025-04-30 09:35:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     ],
     packages=find_packages(),
     include_package_data = True,
-    install_requires = ['asyncio', 'HumanCursor', 'keyboard'],
+    install_requires = ['HumanCursor', 'keyboard'],
     python_requires='>=3.6'
 )
